08c60f1e2878e7e8774a78e88012b7421fc5987a
Ridwan Rahman <ridwanrahman07@gmail.com>
2025-07-02 18:41:34 +1000

    Removed unused folders

--- web_handler/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from models.models import User, Category, UserQuestion, Question
-
-def home_page(request):
-    questions = Question.objects.all()
-    return render(request, "home.html", {"questions": questions})
